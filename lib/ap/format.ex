58aee6184b8fc9141012bd075ae4b4864c86ab66
Björn Rochel <bjoern@bjro.de>
2014-01-18 23:25:03 +0100

    No more aliasing in the tiny protocol code bits

--- lib/ap/format.ex
+++ lib/ap/format.ex
@@ -3,21 +3,17 @@ defprotocol AP.Format do
 end
 
 defimpl AP.Format, for: BitString do
-  import AP.Format.Utils
-  def format(data, _ // []), do: "\"#{data}\"" <> new_line
+  def format(data, _ // []), do: "\"#{data}\"" <> AP.Format.Utils.new_line
 end
 
 defimpl AP.Format, for: Integer do
-  import AP.Format.Utils
-  def format(data, _ // []), do: "#{data}" <> new_line
+  def format(data, _ // []), do: "#{data}" <> AP.Format.Utils.new_line
 end
 
 defimpl AP.Format, for: Atom do
-  import AP.Format.Utils
-  def format(data, _ // []), do: "#{data}" <> new_line
+  def format(data, _ // []), do: "#{data}" <> AP.Format.Utils.new_line
 end
 
 defimpl AP.Format, for: List do
-  import AP.Format.Utils
   def format(data, options // []), do: AP.Format.Seq.format(data, "[", "]", options)
 end
