6e2776b8ec7b4f7554404e6acda0faf89f943fab
Björn Rochel <bjoern@bjro.de>
2014-01-18 20:52:02 +0100

    Taking out comments from the mix file

--- mix.exs
+++ mix.exs
@@ -8,16 +8,10 @@ defmodule AP.Mixfile do
       deps: deps ]
   end
 
-  # Configuration for the OTP application
   def application do
     [mod: { AP, [] }]
   end
 
-  # Returns the list of dependencies in the format:
-  # { :foobar, git: "https://github.com/elixir-lang/foobar.git", tag: "0.1" }
-  #
-  # To specify particular versions, regardless of the tag, do:
-  # { :barbat, "~> 0.1", github: "elixir-lang/barbat" }
   defp deps do
     []
   end
