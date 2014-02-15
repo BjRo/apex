9e49bb3cc46952baebde2c4b83cbe62e3c0f43fe
Björn Rochel <bjoern@bjro.de>
2014-02-15 15:52:47 +0100

    Add a little bit of documentation

--- lib/apex.ex
+++ lib/apex.ex
@@ -1,4 +1,9 @@
 defmodule Apex do
+  @moduledoc "Elixir clone of the awesome_print gem"
+
+  @doc """
+    Pretty prints the supplied data
+  """
   def ap(data, options \\ []) do
     IO.puts(Apex.Format.format(data, options))
   end
