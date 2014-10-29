9c70a65f1c217fd4dcb2f7b3c4936afbc028f169
Björn Rochel <bjoern@bjro.de>
2014-10-29 19:45:00 +0100

    Bump version

--- README.md
+++ README.md
@@ -9,7 +9,7 @@ In order to install it via hex, add the reference to this package into the `deps
 ```elixir
   defp deps do
     [
-      {:apex, "~>0.3.0"}
+      {:apex, "~>0.3.2"}
     ]
   end
 ```
--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Apex.Mixfile do
 
   def project do
     [ app: :apex,
-      version: "0.3.1",
+      version: "0.3.2",
       elixir: "~> 1.0.0",
       build_per_environment: false,
       description: description,
