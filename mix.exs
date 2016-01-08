bb8dbe50537e9333c99762f783af8bb0082bdcb5
Björn Rochel <bjoern@bjro.de>
2016-01-08 21:01:11 +0100

    Update requirement to Elixir 1.2.0

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Apex.Mixfile do
   def project do
     [ app: :apex,
       version: "0.3.4",
-      elixir: "~> 1.1.0",
+      elixir: "~> 1.2.0",
       build_per_environment: false,
       description: description,
       package: package,
