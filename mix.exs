d50120fc347406132786d0e27c06e709f4ffae11
Björn Rochel <bjoern@bjro.de>
2016-01-12 20:25:07 +0100

    Release new version 0.3.6

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Apex.Mixfile do
 
   def project do
     [ app: :apex,
-      version: "0.3.5",
+      version: "0.3.6",
       elixir: "~> 1.2.0",
       build_per_environment: false,
       description: description,
