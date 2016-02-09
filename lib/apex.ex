f47323ec0c999d7fd8326a6d7c7f85020e157182
Björn Rochel <bjoern@bjro.de>
2016-02-09 12:24:27 +0100

    Apex.ap can be used like IO.inspect now

--- lib/apex.ex
+++ lib/apex.ex
@@ -2,9 +2,11 @@ defmodule Apex do
   @moduledoc "Elixir clone of the awesome_print gem"
 
   @doc """
-    Pretty prints the supplied data
+    Pretty prints the supplied data and returns the original data.
   """
   def ap(data, options \\ []) do
-    IO.puts(Apex.Format.format(data, options))
+    formatted = Apex.Format.format(data, options)
+    IO.puts(formatted)
+    data
   end
 end
