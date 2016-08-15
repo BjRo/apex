f0fc80f50ff231883b4af7951ae43be325fcbb62
Bedo Sandor <bedo.sandor@gmail.com>
2016-08-15 22:33:22 +0200

    true, false and nil are not ordinary tagging atoms in tuples

--- lib/apex/format/tuple.ex
+++ lib/apex/format/tuple.ex
@@ -6,15 +6,16 @@ defimpl Apex.Format, for: Tuple do
     do_format(data, options)
   end
 
-  defp do_format({key, value}, options) when is_atom(key) do
+  defp do_format({key, value}, options) when is_atom(key) and not key in [true, false, nil] do
     colorize("#{key}: ", key, options) <> Apex.Format.format(value, options)
   end
 
   defp do_format({}, _options), do: "{}" <> new_line
   defp do_format(data, options) do
     seq = Tuple.to_list(data)
-    if is_atom(hd(seq)) do
-      Seq.format(tl(seq), options, start_token: "#{hd(seq)}{", end_token: "}")
+    head = hd(seq)
+    if is_atom(head) and not head in [true, false, nil] do
+      Seq.format(tl(seq), options, start_token: "#{head}{", end_token: "}")
     else
       Seq.format(Tuple.to_list(data), options, start_token: "{", end_token: "}")
     end
