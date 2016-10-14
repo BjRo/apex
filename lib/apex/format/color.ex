bce50d071924ff45df33eff3e3a5a5ba621851f0
Bjoern Rochel <bjoern.rochel@xing.com>
2016-10-14 22:52:14 +0200

    Can customize colors via ~/.apexrc

--- lib/apex/format/color.ex
+++ lib/apex/format/color.ex
@@ -1,58 +1,84 @@
 defmodule Apex.Format.Color do
-  def color_key(data)
-  def color_key(data) when is_binary(data),   do: {:ok, :binary}
-  def color_key(data) when is_tuple(data),    do: {:ok, :tuple}
-  def color_key(true),                        do: {:ok, true}
-  def color_key(false),                       do: {:ok, false}
-  def color_key(nil),                         do: {:ok, nil}
-  def color_key(data) when is_atom(data),     do: {:ok, :atom}
-  def color_key(data) when is_float(data),    do: {:ok, :float}
-  def color_key(data) when is_integer(data),  do: {:ok, :integer}
-  def color_key(data) when is_function(data), do: {:ok, :function}
-  def color_key(data) when is_list(data),     do: {:ok, :list}
-  def color_key(data) when is_pid(data),      do: {:ok, :pid}
-  def color_key(data) when is_port(data),     do: {:ok, :port}
-  def color_key({Range, _, _}),               do: {:ok, :range}
-  def color_key({HashSet, _, _}),             do: {:ok, :hash_set}
-  def color_key({HashDict, _, _}),            do: {:ok, :hash_dict}
-  def color_key(_),                           do: :error
+  def id(data)
+  def id(data) when is_binary(data),            do: {:ok, :binary}
+  def id(data) when is_tuple(data),             do: {:ok, :tuple}
+  def id(data) when data in [true, false, nil], do: {:ok, data}
+  def id(data) when is_atom(data),              do: {:ok, :atom}
+  def id(data) when is_float(data),             do: {:ok, :float}
+  def id(data) when is_integer(data),           do: {:ok, :integer}
+  def id(data) when is_function(data),          do: {:ok, :function}
+  def id(data) when is_list(data),              do: {:ok, :list}
+  def id(data) when is_pid(data),               do: {:ok, :pid}
+  def id(data) when is_port(data),              do: {:ok, :port}
+  def id({Range, _, _}),                        do: {:ok, :range}
+  def id({HashSet, _, _}),                      do: {:ok, :hash_set}
+  def id({HashDict, _, _}),                     do: {:ok, :hash_dict}
+  def id(_),                                    do: :unknown
 
-  def color_value(key)
-  def color_value(:binary),    do: {:ok, :yellow}
-  def color_value(:tuple),     do: {:ok, :blue}
-  def color_value(:true),      do: {:ok, [:green, :bright]}
-  def color_value(:false),     do: {:ok, [:red, :bright]}
-  def color_value(:nil),       do: {:ok, :red}
-  def color_value(:atom),      do: {:ok, :cyan}
-  def color_value(:float),     do: {:ok, :blue}
-  def color_value(:integer),   do: {:ok, :blue}
-  def color_value(:function),  do: {:ok, :magenta}
-  def color_value(:list),      do: {:ok, :white}
-  def color_value(:pid),       do: {:ok, :yellow}
-  def color_value(:port),      do: {:ok, [:green, :bright]}
-  def color_value(:range),     do: {:ok, :green}
-  def color_value(:hash_set),  do: {:ok, :white}
-  def color_value(:hash_dict), do: {:ok, :white}
-  def color_value(_),          do: :error
+  @default_colors %{
+    binary:    :yellow,
+    tuple:     :blue,
+    true:      [:green, :bright],
+    false:     [:red, :bright],
+    nil:       :red,
+    atom:      :cyan,
+    float:     :blue,
+    integer:   :blue,
+    function:  :magenta,
+    list:      :white,
+    pid:       :yellow,
+    port:      [:green, :bright],
+    range:     :green,
+    hash_set:  :white,
+    hash_dict: :white
+  }
+
+  def default_color(id) do
+    Map.get(@default_colors, id)
+  end
+
+  def customization_file do
+    Path.expand("~/.apexrc")
+  end
+
+  def load_color_customizations do
+    with {:ok, contents }        <- File.read(customization_file),
+         {%{colors: colors}, _}  <- Code.eval_string(contents, [], __ENV__) do
+           colors
+         else
+           _ -> %{}
+         end
+  end
 
   defmacro __using__(_) do
+    user_defined_colors = __MODULE__.load_color_customizations
+
     quote do
-      def colorize(text, data, options \\ []) do
-        cond do
-          options[:color] == false   -> text
-          {:ok, color} = color(data) -> escape(text, color)
-          true                       -> text
-        end
+      import unquote(__MODULE__)
+
+      def colorize(text, data, opts \\ []) do
+        data
+          |> id
+          |> do_colorize(text, opts[:color])
       end
 
       def escape(text, color) do
-        IO.ANSI.format([color, text], true) |> IO.chardata_to_string
+        [color, text]
+          |> IO.ANSI.format(true)
+          |> IO.chardata_to_string
       end
 
-      def color(data) do
-        with {:ok, key} = unquote(__MODULE__).color_key(data) do
-          unquote(__MODULE__).color_value(key)
-        end
+      defp do_colorize(id, text, should_color)
+      defp do_colorize(_id, text, false),  do: text
+      defp do_colorize(:unknown, text, _), do: text
+      defp do_colorize({:ok, id}, text, _) do
+        color = user_defined_color(id) || default_color(id)
+        escape(text, color)
+      end
+
+      @color_customizations unquote(Macro.escape(user_defined_colors))
+      defp user_defined_color(id) do
+        Map.get(@color_customizations, id)
       end
     end
   end
